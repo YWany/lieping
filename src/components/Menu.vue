642c052720d296d423a0dfe19871ad152e55e50a
chengangqiang <>
2018-06-26 13:31:26 +0800

    test

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -29,44 +29,65 @@
         <Submenu name="2">
             <template slot="title">
                 <Icon type="ios-filing"></Icon>
-                Navigation <Two></Two>
+                财务 <Two></Two>
             </template>
-                    <MenuItem name="2-1">Option 5</MenuItem>
-            <MenuItem name="2-2">Option 6</MenuItem>
-            <Submenu name="3">
-                <template slot="title">Submenu</template>
-                <MenuItem name="3-1">Option 7</MenuItem>
-                <MenuItem name="3-2">Option 8</MenuItem>
-            </Submenu>
+            <MenuItem name="2-1">发票管理</MenuItem>
+            <MenuItem name="2-2">付款通知书</MenuItem>
         </Submenu>
         <Submenu name="3">
             <template slot="title">
                 <Icon type="ios-filing"></Icon>
-                Navigation Three
+                管理
             </template>
-            <MenuItem name="2-11">Option 5</MenuItem>
-            <MenuItem name="2-21">Option 6</MenuItem>
+            <MenuItem name="2-11">新增账号</MenuItem>
+            <MenuItem name="2-21">权限管理</MenuItem>
+            <MenuItem name="2-222">部门管理</MenuItem>
         </Submenu>
         <Submenu name="2">
             <template slot="title">
                 <Icon type="ios-filing"></Icon>
-                Navigation Two
+                审核
             </template>
-            <MenuItem name="2-1">Option 5</MenuItem>
-            <MenuItem name="2-2">Option 6</MenuItem>
-            <Submenu name="3">
+            <MenuItem name="2-1">合同</MenuItem>
+            <MenuItem name="2-2">订单</MenuItem>
+            <MenuItem name="2-3">发票</MenuItem>
+            <MenuItem name="2-4">所有审批</MenuItem>
+            <!-- <Submenu name="3">
                 <template slot="title">Submenu</template>
                 <MenuItem name="3-1">Option 7</MenuItem>
                 <MenuItem name="3-2">Option 8</MenuItem>
-            </Submenu>
+            </Submenu> -->
         </Submenu>
         <Submenu name="3">
             <template slot="title">
-                <Icon type="ios-filing"></Icon>
-                Navigation Three
+                <Icon type="social-apple"></Icon>
+                报表
             </template>
-            <MenuItem name="2-11">Option 5</MenuItem>
-            <MenuItem name="2-21">Option 6</MenuItem>
+            <!-- <MenuItem name="2-7">Option 5</MenuItem> -->
+            <Submenu name="3">
+                <template slot="title">订单</template>
+                <MenuItem name="3-1">个人报表</MenuItem>
+                <MenuItem name="3-2">部门报表</MenuItem>
+                <MenuItem name="3-3">所有报表</MenuItem>
+            </Submenu>
+            <Submenu name="4">
+                <template slot="title">合同</template>
+                <MenuItem name="4-1">个人报表</MenuItem>
+                <MenuItem name="4-2">部门报表</MenuItem>
+                <MenuItem name="4-3">所有报表</MenuItem>
+            </Submenu>
+             <Submenu name="5">
+                <template slot="title">项目</template>
+                <MenuItem name="5-1">个人报表</MenuItem>
+                <MenuItem name="5-2">部门报表</MenuItem>
+                <MenuItem name="5-3">所有报表</MenuItem>
+            </Submenu>
+             <Submenu name="6">
+                <template slot="title">推荐人选</template>
+                <MenuItem name="6-1">个人报表</MenuItem>
+                <MenuItem name="6-2">部门报表</MenuItem>
+                <MenuItem name="6-3">所有报表</MenuItem>
+            </Submenu>
         </Submenu>
     </Menu>
 </div>

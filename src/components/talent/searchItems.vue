2308e25752a96d2509efee44324996f279be125e
chengangqiang <>
2018-07-26 11:12:15 +0800

    no message

--- src/components/talent/searchItems.vue
+++ src/components/talent/searchItems.vue
@@ -9,24 +9,24 @@
                 <!-- <Input v-model="formValidate.industryId"></Input> -->
             </FormItem>
             <FormItem label="目前工作地区" prop="areaName">
-                 <Citysels ref='proCity' />
-            </FormItem>
-            <FormItem label="期望工作地区" prop="applyUserName">
                 <Citysels ref='proCity' />
             </FormItem>
+            <!-- <FormItem label="期望工作地区" prop="applyUserName">
+                <Citysels ref='proCity' />
+            </FormItem> -->
             <FormItem label="关键词">
                 <Input v-model="formValidate.keyword"></Input>
             </FormItem>
             <FormItem label="公司名称" prop="companyName">
                 <Input v-model="formValidate.companyName" placeholder="请输入公司名称"></Input>
-                <RadioGroup v-model="formValidate.comtype" style='margin-left:10px;'>
+                <!-- <RadioGroup v-model="formValidate.comtype" style='margin-left:10px;'>
                     <Radio label="apple">
                         <span>目前公司</span>
                     </Radio>
                     <Radio label="android">
                         <span>全部经历</span>
                     </Radio>
-                </RadioGroup>
+                </RadioGroup> -->
             </FormItem>
             <FormItem label="职能类别" prop="applyUserName">
                 <Input v-model="formValidate.applyUserName"></Input>
@@ -57,8 +57,8 @@
             </FormItem>
             <FormItem label="年龄" prop="applyUserName">
                 <Select value='' placeholder="请选择年龄" class='liSel'>
-                    <Option value='1'>1年</Option>
-                    <Option value='2'>2年</Option>
+                    <Option value='1'>18年</Option>
+                    <Option value='2'>19年</Option>
                     <Option value='3'>3年</Option>
                 </Select> -
                 <Select value='' placeholder="请选择年龄" class='liSel'>
@@ -67,14 +67,17 @@
                     <Option value='3'>3年</Option>
                 </Select>
             </FormItem>
-            <FormItem label="性别" prop="applyUserName">
-                <Select value='' placeholder="请选择性别">
-                    <Option value='1'>男</Option>
-                    <Option value='2'>女</Option>
+            <FormItem label="性别">
+                <Select v-model="formValidate.resumeGender" placeholder="请选择性别">
+                    <Option value='male'>男</Option>
+                    <Option value='female'>女</Option>
                 </Select>
             </FormItem>
-            <FormItem label="更新时间" prop="applyUserName">
-                <DatePicker type="datetime" :value='formValidate.followTime' format="yyyy-MM-dd HH:mm:ss" @on-change='seltime'></DatePicker>
+            <FormItem label="更新时间" prop="dateModified">
+                <Select v-model="formValidate.dateModified" placeholder="请选择性别">
+                    <Option value='male'>男</Option>
+                    <Option value='female'>女</Option>
+                </Select>
             </FormItem>
             <FormItem label="求职状态" prop="applyUserName">
                 <Select value='' placeholder="请选择求职状态">
@@ -83,58 +86,62 @@
                 </Select>
             </FormItem>
             <!-- <FormItem label="合同序号" prop="contractId">
-                <Select v-model="formValidate.contractId" placeholder="请选择合同序号">
-                    <Option v-for="(item,index) in hetongLists" :value="item.id" :key='index'>{{item.num}}</Option>
-                </Select>
-            </FormItem> -->
+                            <Select v-model="formValidate.contractId" placeholder="请选择合同序号">
+                                <Option v-for="(item,index) in hetongLists" :value="item.id" :key='index'>{{item.num}}</Option>
+                            </Select>
+                        </FormItem> -->
         </Form>
         <div class="search" style='text-align:center;'>
-            <Button type='info' @click="handleSubmit('formValidate')">搜索人才</Button>
+            <!-- <Button type='info' @click="handleSubmit('formValidate')">搜索人才</Button> -->
+            <Button type="primary" :loading="loading" @click="handleSubmit(1)">
+                <span v-if="!loading">搜索人才</span>
+                <span v-else>Loading...</span>
+            </Button>
         </div>
 
         <!-- 新建分组 -->
         <!-- <div class="addGroupPop">
-            <Modal v-model="addGroupPop" :closable='false' :mask-closable='false' style='text-align:center'>
-                <div slot='header' style='font-size:14px;color:#444'>
-                    新建分组
-                    <a href="javascript:;" @click='addGroupPop=false'>
-                        <Icon type="close" class='fr'></Icon>
-                    </a>
-                </div>
-                <span>分组名称： </span>
-                <Input v-model='addgroupForm.folderName' placeholder='请输入分组名称' style='width:300px;padding:20px 0'></Input>
-                <div slot='footer' style='text-align:center'>
-                    <Button type='info' @click='subGroupSave'>保存</Button>
-                </div>
-            </Modal>
-        </div> -->
+                        <Modal v-model="addGroupPop" :closable='false' :mask-closable='false' style='text-align:center'>
+                            <div slot='header' style='font-size:14px;color:#444'>
+                                新建分组
+                                <a href="javascript:;" @click='addGroupPop=false'>
+                                    <Icon type="close" class='fr'></Icon>
+                                </a>
+                            </div>
+                            <span>分组名称： </span>
+                            <Input v-model='addgroupForm.folderName' placeholder='请输入分组名称' style='width:300px;padding:20px 0'></Input>
+                            <div slot='footer' style='text-align:center'>
+                                <Button type='info' @click='subGroupSave'>保存</Button>
+                            </div>
+                        </Modal>
+                    </div> -->
 
         <!-- 分组列表 -->
         <!-- <div class="listGroupPop">
-            <Modal v-model="listGroupPop" :closable='false' :mask-closable='false' style='text-align:center;' width='360px'>
-                <div slot='header' style='font-size:14px;color:#444'>
-                    新建分组
-                    <a href="javascript:;" @click='listGroupPop=false'>
-                        <Icon type="close" class='fr'></Icon>
-                    </a>
-                </div>
-                <Input v-model="addgroupForm.folderName" @on-enter='searchIn' placeholder="请输入分组名称" style='margin:20px auto;width:300px;'>
-                    <Button slot="append" @click='searchIn'>新增</Button>
-                </Input>
-                <div class="sels">
-                    <RadioGroup v-model="selgroupForm.folderId">
-                        <Radio label="金斑蝶"></Radio>
-                        <Radio label="爪哇犀牛"></Radio>
-                        <Radio label="印度黑羚"></Radio>
-                        <Radio label="金斑蝶"></Radio>
-                        <Radio label="爪哇犀牛"></Radio>
-                    </RadioGroup>
-                </div>
-                <div slot='footer' style='text-align:center'>
-                    <Button type='info' @click='subGroupSave'>确定</Button>
-                </div>
-            </Modal>
-        </div> -->
+                        <Modal v-model="listGroupPop" :closable='false' :mask-closable='false' style='text-align:center;' width='360px'>
+                            <div slot='header' style='font-size:14px;color:#444'>
+                                新建分组
+                                <a href="javascript:;" @click='listGroupPop=false'>
+                                    <Icon type="close" class='fr'></Icon>
+                                </a>
+                            </div>
+                            <Input v-model="addgroupForm.folderName" @on-enter='searchIn' placeholder="请输入分组名称" style='margin:20px auto;width:300px;'>
+                                <Button slot="append" @click='searchIn'>新增</Button>
+                            </Input>
+                            <div class="sels">
+                                <RadioGroup v-model="selgroupForm.folderId">
+                                    <Radio label="金斑蝶"></Radio>
+                                    <Radio label="爪哇犀牛"></Radio>
+                                    <Radio label="印度黑羚"></Radio>
+                                    <Radio label="金斑蝶"></Radio>
+                                    <Radio label="爪哇犀牛"></Radio>
+                                </RadioGroup>
+                            </div>
+                            <div slot='footer' style='text-align:center'>
+                                <Button type='info' @click='subGroupSave'>确定</Button>
+                            </div>
+                        </Modal>
+                    </div> -->
         <Professions ref='professionComp' @selPro='selPro' :professPop='professPop' />
 
     </div>
@@ -149,7 +156,7 @@ import Professions from "@/components/common/professions.vue";
 export default {
     name: "serachItems",
     components: {
-         Citysels,
+        Citysels,
         Professions
     },
     data() {
@@ -158,6 +165,7 @@ export default {
             listGroupPop: false,
             groupLists: [], //分组列表
             professPop: false, //职位弹窗
+            loading: false,
             professName: "",
             addgroupForm: {
                 userId: ls.get("accid"),
@@ -166,14 +174,26 @@ export default {
             selgroupForm: {
                 folderId: ""
             },
+            formdata: {
+                total: 100,
+                pageNum: 1,
+                pageSize: 10
+            },
             formValidate: {
-                position: "",
-                industryId: "",
-                companyName: "",
-                keyword: "",
-                comtype: "",
-                contractId: "",
-                fundType: ""
+                position: "前端工程师", //岗位
+                keyword: "", //关键词
+                ageFrom: "", //年龄下限(不能小于18)
+                ageTo: "", //年龄上限(不能大于60)
+                workYearFrom: "", //工作经验下限
+                workYearTo: "", //工作经验上限
+                eduForm: "", //学历下限
+                eduTo: "", //学历上限
+                dateModified: "", //简历更新时间
+                schoolName: "", //学校名
+                resumeGender: "", //性别
+                areaId: "", //区域id
+                industryId: "2002", //行业id
+                companyName: "千里马" //公司名称
             },
             ruleValidate: {
                 // position: [
@@ -202,56 +222,98 @@ export default {
     },
     computed: {},
     methods: {
-        getGroupLists() {
-            //获取分组列表
-            api.axs("post", "/userFolder/folderList").then(({ data }) => {
-                if (data.code === "SUCCESS") {
-                    this.groupLists = data.data;
-                } else {
-                    this.$Message.error(data.remark);
-                }
-            });
-        },
-        subGroupSave() {
-            //选择分组
-            if (!this.selgroupForm.folderId) {
-                this.$Message.warning("请一个分组！");
-                return;
-            }
+        // getGroupLists() {
+        //     //获取分组列表
+        //     api.axs("post", "/userFolder/folderList").then(({
+        //         data
+        //     }) => {
+        //         if (data.code === "SUCCESS") {
+        //             this.groupLists = data.data;
+        //         } else {
+        //             this.$Message.error(data.remark);
+        //         }
+        //     });
+        // },
+        // subGroupSave() {
+        //     //选择分组
+        //     if (!this.selgroupForm.folderId) {
+        //         this.$Message.warning("请一个分组！");
+        //         return;
+        //     }
+        //     api
+        //         .axs("post", "/userFolder/addUserFolder", this.addgroupForm)
+        //         .then(({ data }) => {
+        //             if (data.code === "SUCCESS") {
+        //                 this.$Message.success("新增成功!");
+        //             } else {
+        //                 this.$Message.error(data.remark);
+        //             }
+        //         });
+        // },
+        handleSubmit(page) {
+            console.log(page)
+            //搜素人才
+            // if (this.$refs.proCity.cityId === "") {
+            //     this.companyForm.areaId = this.$refs.proCity.proId;
+            // } else {
+            //     this.companyForm.areaId = this.$refs.proCity.cityId;
+            // }
+            this.loading = true;
+            this.$store.state.spinShow = true;
             api
-                .axs("post", "/userFolder/addUserFolder", this.addgroupForm)
+                .axs("post", "/resume/search", this.formValidate)
                 .then(({ data }) => {
+                    this.$store.state.spinShow = false;
                     if (data.code === "SUCCESS") {
-                        this.$Message.success("新增成功!");
+                        this.createpop();
                     } else {
                         this.$Message.error(data.remark);
                     }
                 });
         },
-        handleSubmit(name) {
-            //新增分组
-             if (this.$refs.proCity.cityId === "") {
-                    this.companyForm.areaId = this.$refs.proCity.proId;
-                } else {
-                    this.companyForm.areaId = this.$refs.proCity.cityId;
-                }
-            this.$refs[name].validate(valid => {
-                if (valid) {
-                    this.$store.state.spinShow = true;
-                    api
-                        .axs("post", "/resume/search", this.formValidate)
-                        .then(({ data }) => {
-                            this.$store.state.spinShow = false;
-                            if (data.code === "SUCCESS") {
-                                this.$Message.success("新增成功!");
-                            } else {
-                                this.$Message.error(data.remark);
-                            }
-                        });
-                } else {
-                    this.$Message.warning("请填写完整!");
-                }
-            });
+        createpop() {
+            //创建任务
+            var vm = this;
+            window.timer = setInterval(function() {
+                api
+                    .axs("post", "/resume/pollingTaskSchedule")
+                    .then(({ data }) => {
+                        if (data.code === "SUCCESS" && data.data == 3) {
+                            console.log(data);
+                            window.clearInterval(window.timer);
+                            api
+                                .axs("post", "/resume/getResumeList", vm.formdata)
+                                .then(({ data }) => {
+                                    if (data.code === "SUCCESS") {
+                                        console.log(data);
+                                        vm.loading = false;
+                                        vm.$parent.data1 = data.data.list;
+                                        vm.$parent.form.total = data.data.total;
+                                    } else {
+                                        vm.$Message.error(data.remark);
+                                    }
+                                });
+                        } else if (data.code === "FAIL") {
+                            this.$Message.error(data.remark);
+                            window.clearInterval(window.timer);
+                        }
+                    });
+            }, 1000);
+        },
+        search2Lits(page) {
+            this.formdata.pageNum = page
+            api
+                .axs("post", "/resume/getResumeList", vm.formdata)
+                .then(({ data }) => {
+                    if (data.code === "SUCCESS") {
+                        console.log(data);
+                        vm.loading = false;
+                        vm.$parent.data1 = data.data.list;
+                        vm.$parent.form.total = data.data.total;
+                    } else {
+                        vm.$Message.error(data.remark);
+                    }
+                });
         },
         selPro() {
             //选择职位
@@ -270,7 +332,7 @@ export default {
         }
     },
     mounted() {
-        this.getGroupLists();
+        // this.getGroupLists();
     }
 };
 </script>
